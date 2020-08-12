8b4ef109ee894383f8a954e31257758f7940d9c2
matthew <matthewdicks98@gmail.com>
2020-08-12 13:09:09 +0200

    Updated the packet_byte_extractor

--- preprocessing/packet-byte-extracton/packet_byte_extractor.py
+++ preprocessing/packet-byte-extracton/packet_byte_extractor.py
@@ -3,7 +3,15 @@ from scapy.all import *
 import time
 
 def main():
-    # packets per label = 10 000
+
+    # dictionary with the number of packets per label
+    packets_per_label = {}
+
+    # getting a list with the labels already used
+    labels_used = []
+
+    # setting the labels that we want to exclude
+    excluded_labels = ['HTTP','SSDP','Unknown','TLS','HTTP_Proxy']
 
     # set the file path 
     cwd = os.getcwd()
@@ -19,7 +27,7 @@ def main():
 
     # write the headers
     header = ['label']
-    for i in range(1, 1501):
+    for i in range(1, 1481):
         header.append('byte'+str(i))
     f_write.write(','.join(header)+'\n')
 
@@ -32,11 +40,19 @@ def main():
                 # get the file path, labels and num packets from csv
                 file_path = split_line[0]
                 label = split_line[1]
+
+                # if the label has not been used yet initialize
+                if label not in labels_used:
+                    packets_per_label[label] = 0
+
                 num_packets = split_line[2]
                 file_path = directory_path + file_path
 
                 # process the pcap file
-                label_packets(file_path, label, f_write)
+                if label not in excluded_labels:
+                    packets_per_label = label_packets(file_path, label, f_write, packets_per_label)
+                else:
+                    continue
 
                 print('{} {} has been processed'.format(count, file_path))
                 count += 1
@@ -52,38 +68,45 @@ def main():
     # close file for writing
     f_write.close()
 
-def label_packets(file_path, label, f_write):
+def label_packets(file_path, label, f_write, packets_per_label):
     '''
     param file_path (String): Path to the pcap file
     param label(String): Label for each packet in the flow 
     '''
+    # packets per label = 100 000
+    maximum_packets = 100000
 
     # This value will determine how many packets per flow(with payload) to sample
     # set to ALL to sample all packets in a flow
-    packets = 0
-    packets_per_flow = 10000
-    count_packets_per_flow = 0
-    if os.path.exists(file_path):
-        data = rdpcap(file_path)
-        #print('Read {}'.format(file_path))
-        sessions = data.sessions()
-        for session in sessions:
-            for pkt in sessions[session]:
-                if count_packets_per_flow <= packets_per_flow:
-                    if pkt.haslayer(IP) and pkt.haslayer(Raw):
-                        hex_data = linehexdump(pkt[IP].payload, onlyhex=1, dump=True).split(" ")
-                        decimal_data = list(map(hex_to_dec, hex_data))
-                        #print(decimal_data)
-                        f_write.write(label+','+','.join(decimal_data))
-                        f_write.write('\n')
+    if packets_per_label[label] < maximum_packets:
+        packets = 0
+        packets_per_flow = 10000
+        count_packets_per_flow = 0
+        if os.path.exists(file_path):
+            data = rdpcap(file_path)
+            #print('Read {}'.format(file_path))
+            sessions = data.sessions()
+            for session in sessions:
+                for pkt in sessions[session]:
+                    if count_packets_per_flow <= packets_per_flow:
+                        if pkt.haslayer(IP) and pkt.haslayer(Raw):
+                            hex_data = linehexdump(pkt[IP].payload, onlyhex=1, dump=True).split(" ")
+                            decimal_data = list(map(hex_to_dec, hex_data))
+                            #print(decimal_data)
+                            f_write.write(label+','+','.join(decimal_data))
+                            f_write.write('\n')
+                            
+                            packets_per_label[label] += 1
+                        else:
+                            continue
                     else:
-                        continue
-                else:
-                    break
-                count_packets_per_flow += 1
+                        break
+                    count_packets_per_flow += 1
 
-    else:
-        print('Could not find {}'.format(file_path))
+        else:
+            print('Could not find {}'.format(file_path))
+
+    return packets_per_label
 
 def hex_to_dec(hex):
     return str(int(hex, base = 16))
