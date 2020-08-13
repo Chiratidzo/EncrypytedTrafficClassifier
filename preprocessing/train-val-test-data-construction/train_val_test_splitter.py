583b6373fd4c9f4cba777422ea37c42416500e79
Shane Weisz <shaneweisz@gmail.com>
2020-08-13 10:54:25 +0200

    Changed to train-val-test split (added val set)

--- preprocessing/train-val-test-data-construction/train_val_test_splitter.py
+++ preprocessing/train-val-test-data-construction/train_val_test_splitter.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sklearn
 from sklearn.model_selection import train_test_split
 import logging
 
@@ -14,7 +15,7 @@ logger = logging.getLogger("logger")
 
 # Read in the csv with byte_array - label pairs
 logger.info(
-    "Reading in the csv with all packets - will take approximately 3 minutes...")
+    "Reading in the csv with all packets - will take approximately 2 minutes...")
 df = pd.read_csv('../data/data.csv')
 logger.info("Reading csv complete")
 
@@ -33,15 +34,22 @@ for label in CLASSES:
 # Zero-pad packets that have fewer than 1480 bytes in the packets
 df_new.fillna(0, inplace=True)
 df_train, df_test = train_test_split(df_new, test_size=0.2, random_state=42)
+df_train, df_val = train_test_split(df_train, test_size=0.2, random_state=42)
 logger.info("Zero-padding the packets")
 
 # Write to the train and test csv's named using the format:
 #   [NUM_CLASSES]_[PACKETS_PER_CLASS]_['train'|'test'].csv
+logger.info("Writing to csv's")
+
 file_suffix = "{0}_{1}".format(len(CLASSES), PACKETS_PER_CLASS)  # e.g. 3_10000
 
 df_train.to_csv(
     "../../data/{0}/train_{0}.csv".format(file_suffix), index=False)
 logger.info("train_{0}.csv created".format(file_suffix))
+
+df_val.to_csv(
+    "../../data/{0}/val_{0}.csv".format(file_suffix), index=False)
+logger.info("val_{0}.csv created".format(file_suffix))
 
 df_test.to_csv("../../data/{0}/test_{0}.csv".format(file_suffix), index=False)
 logger.info("test_{0}.csv created".format(file_suffix))
