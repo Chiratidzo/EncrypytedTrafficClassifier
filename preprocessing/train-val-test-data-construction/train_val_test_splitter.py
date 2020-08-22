95b616fbff01fdfe3994f90e24d1e744f5a378c6
Shane Weisz <shaneweisz@gmail.com>
2020-08-22 15:01:17 +0200

    Full preprocessed dataset - 12 classes, 10000 per class

--- preprocessing/train-val-test-data-construction/train_val_test_splitter.py
+++ preprocessing/train-val-test-data-construction/train_val_test_splitter.py
@@ -6,8 +6,10 @@ import logging
 import os
 
 
-CLASSES = ["YouTube", "Facebook", "WhatsApp", "Microsoft", "Amazon", "Google"]
-PACKETS_PER_CLASS = 5000
+CLASSES = ["YouTube", "Facebook", "GoogleServices", "Instagram", "WhatsApp", "BitTorrent",
+           "TeamViewer", "GMail", "Cloudflare", "WindowsUpdate", "PlayStore", "GoogleDocs"]
+PACKETS_PER_CLASS = 10000
+RANDOM_STATE = 42
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s  %(message)s',
@@ -15,28 +17,35 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger("logger")
 
 # Read in the csv with byte_array - label pairs
-logger.info(
-    "Reading in the csv with all packets - will take approximately 2 minutes...")
+logger.info("Reading in the csv with all packets")
 df = pd.read_csv('./data/data.csv')
 logger.info("Reading csv complete")
 
 # Sample PACKETS_PER_CLASS packets from each class in CLASSES and add to df_new
 df_new = pd.DataFrame()
 for label in CLASSES:
+    logger.info("Sampling {0} packets from {1}".format(PACKETS_PER_CLASS,
+                                                       label))
     # e.g. if label == Facebook, extract the Facebook rows
     df_label = df[df["label"] == label]
     # e.g. make sure at least 10000 Facebook packets
     assert len(df_label) >= PACKETS_PER_CLASS
-    df_sampled = df_label.sample(n=PACKETS_PER_CLASS, random_state=42)
+    df_sampled = df_label.sample(n=PACKETS_PER_CLASS,
+                                 random_state=RANDOM_STATE)
     df_new = df_new.append(df_sampled)
-    logger.info("Sampling {0} packets from {1}".format(
-        PACKETS_PER_CLASS, label))
 
 # Zero-pad packets that have fewer than 1480 bytes in the packets
-df_new.fillna(0, inplace=True)
-df_train, df_test = train_test_split(df_new, test_size=0.2, random_state=42)
-df_train, df_val = train_test_split(df_train, test_size=0.2, random_state=42)
 logger.info("Zero-padding the packets")
+df_new.fillna(0, inplace=True)
+
+# Split into train, val and test sets, using stratified splits to preserve balance in classes
+logger.info("Splitting into training, validation and testing sets")
+df_train, df_test = train_test_split(df_new, test_size=0.2,
+                                     random_state=RANDOM_STATE,
+                                     stratify=df_new["label"])
+df_train, df_val = train_test_split(df_train, test_size=0.2,
+                                    random_state=RANDOM_STATE,
+                                    stratify=df_train["label"])
 
 # Write to the train and test csv's named using the format:
 #   [NUM_CLASSES]_[PACKETS_PER_CLASS]_['train'|'test'].csv
@@ -49,13 +58,14 @@ try:
 except OSError as error:
     print(error)
 
-df_train.to_csv(
-    "./../data/{0}/train_{0}.csv".format(file_suffix), index=False)
+df_train.to_csv("./../data/{0}/train_{0}.csv".format(file_suffix),
+                index=False)
 logger.info("train_{0}.csv created".format(file_suffix))
 
-df_val.to_csv(
-    "./../data/{0}/val_{0}.csv".format(file_suffix), index=False)
+df_val.to_csv("./../data/{0}/val_{0}.csv".format(file_suffix),
+              index=False)
 logger.info("val_{0}.csv created".format(file_suffix))
 
-df_test.to_csv("./../data/{0}/test_{0}.csv".format(file_suffix), index=False)
+df_test.to_csv("./../data/{0}/test_{0}.csv".format(file_suffix),
+               index=False)
 logger.info("test_{0}.csv created".format(file_suffix))
